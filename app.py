8cb7f157afe150b65e33701e94bd9f4ee2a9338f
andrearm20 <armoreno2016@gmail.com>
2025-05-05 16:24:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -54,42 +54,42 @@ if st.button("Cargar y analizar datos"):
     st.dataframe(outliers)
     
 def get_temperature_data():
-    query2 = '''
+    query = '''
     from(bucket: "homeiot")
       |> range(start: -24h)
       |> filter(fn: (r) => r._measurement == "airSensor")
       |> filter(fn: (r) => r._field == "humidity")
     '''
-    client2 = InfluxDBClient(url=INFLUX_URL, token=INFLUX_TOKEN, org=ORG)
-    df2 = client.query_api().query_data_frame(org=ORG, query2=query2)
-    df2 = df2[["_time", "_value"]].rename(columns={"_time": "timestamp", "_value": "humedad"})
-    df2["timestamp"] = pd.to_datetime(df["timestamp"])
-    return df2
+    client = InfluxDBClient(url=INFLUX_URL, token=INFLUX_TOKEN, org=ORG)
+    df = client.query_api().query_data_frame(org=ORG, query=query)
+    df = df[["_time", "_value"]].rename(columns={"_time": "timestamp", "_value": "humidity"})
+    df["timestamp"] = pd.to_datetime(df["timestamp"])
+    return df
 # --- Detección de anomalías con Isolation Forest ---
-def detectar_anomalias(df2):
-    model2 = IsolationForest(contamination=0.1, random_state=40)
-    df2["anomaly"] = model2.fit_predict(df2[["humedad"]])
-    return df2
+def detectar_anomalias(df):
+    model = IsolationForest(contamination=0.05, random_state=42)
+    df["anomaly"] = model.fit_predict(df[["humidity"]])
+    return df
 
 # --- Streamlit UI ---
-st.title("Análisis de humedad con IA local")
+st.title("Análisis de Temperatura con IA local")
 if st.button("Cargar y analizar datos"):
-    df2 = get_temperature_data()
+    df = get_temperature_data()
     st.subheader("Datos crudos:")
-    st.dataframe(df2)
+    st.dataframe(df)
 
     st.subheader("Estadísticas descriptivas:")
-    st.write(df2["humedad"].describe())
+    st.write(df["humidity"].describe())
 
-    df2 = detectar_anomalias(df2)
-    outliers2 = df2[df2["anomaly"] == -1]
+    df = detectar_anomalias(df)
+    outliers = df[df["anomaly"] == -1]
 
     st.subheader("Visualización con anomalías:")
-    fig2, ax = plt.subplots()
-    sns.lineplot(x="timestamp", y="humedad", data=df, label="humedad", ax=ax)
-    ax.scatter(outliers["timestamp"], outliers["humedad"], color="red", label="Anomalía", zorder=5)
+    fig, ax = plt.subplots()
+    sns.lineplot(x="timestamp", y="humidity", data=df, label="humidity", ax=ax)
+    ax.scatter(outliers["timestamp"], outliers["humidity"], color="red", label="Anomalía", zorder=5)
     ax.legend()
-    st.pyplot(fig2)
+    st.pyplot(fig)
 
     st.subheader("Anomalías detectadas:")
     st.dataframe(outliers)
